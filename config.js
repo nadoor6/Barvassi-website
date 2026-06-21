72f1f68b9e70e4f1e122a7f0850c27e4bd1626eb
nadoor6 <nadernader777111@gmail.com>
2026-06-21 11:11:56 +0300

    1

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// Your keys are safely stored here and automatically loaded by the pages
+const SUPABASE_URL = "https://inycllornehmohderdfz.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Bnkek18bbEnBgnis-nUVKw_QABqaYKj";
