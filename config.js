2dbcbf8a5b0000bb8b68319aa938f827dc013fab
nadoor6 <nadernader777111@gmail.com>
2026-06-22 01:53:54 +0300

    1

--- config.js
+++ config.js
@@ -1,2 +1,3 @@
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// config.js
+const SUPABASE_URL = "https://inycllornehmohderdfz.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_Bnkek18bbEnBgnis-nUVKw_QABqaYKj";
